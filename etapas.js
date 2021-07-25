580112716be2301411caaa98e5940be32efbf69e
Max <maxsoncoelho@gmail.com>
2021-07-25 15:21:00 -0400

    dynamic numbers and finding candidate

--- etapas.js
+++ etapas.js
@@ -1,23 +1,48 @@
 let etapas = [
     {
-        titulo: 'PRESIDENTE   ',
+        titulo: 'PRESIDENTE',
+        numeros: 3,
         candidatos: [
             {
-                numero: 38111,
+                numero: '380',
                 nome: 'Jair Messias Bolsonabo',
-                partido: 'ABC',
+                partido: 'Armas e livros',
                 fotos:[
                     {url:'Bolsonaro.jpg', legenda: 'Presidente'},
                     {url:'Mourao.jpg', legenda: 'Vice-Presidente', small: true}
                 ]
             },
             {
-                numero: 666,
+                numero: '666',
                 nome: 'Luiz Inácio Cachaceiro da Silva',
-                partido: 'ABC',
+                partido: 'Partido das Trevas',
                 fotos:[
                     {url:'Lula.jpeg', legenda: 'Presidente'},
                     {url:'Dilma.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'GOVERNADOR',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '11',
+                nome: 'Amazonino Mendes',
+                partido: 'Koreia do Norte',
+                fotos:[
+                    {url:'Amazonino.jpg', legenda: 'Governador'},
+                    {url:'Sereia do Bodozal.jpg', legenda: 'Vice-Governador', small: true}
+                ]
+            },
+            {
+                numero: '24',
+                nome: 'Omar Azis',
+                partido: 'Circo',
+                fotos:[
+                    {url:'Omar.jpeg', legenda: 'Governador'},
+                    {url:'Ruivinha de Marte.jpg', legenda: 'Vice-Governador', small: true}
                 ]
             },
         ]
