12da4d19b6d516be8ae5e4ddf7373ea60e472685
Max <maxsoncoelho@gmail.com>
2021-07-25 10:52:39 -0400

    html e css ok, more variables from ambients

---
+++ etapas.js
@@ -0,0 +1,26 @@
+let etapas = [
+    {
+        titulo: 'PRESIDENTE   ',
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Jair Messias Bolsonabo',
+                partido: 'ABC',
+                fotos:[
+                    {url:'Bolsonaro.jpg', legenda: 'Presidente'},
+                    {url:'Mourao.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: 666,
+                nome: 'Luiz Inácio Cachaceiro da Silva',
+                partido: 'ABC',
+                fotos:[
+                    {url:'Lula.jpeg', legenda: 'Presidente'},
+                    {url:'Dilma.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+        ]
+    },
+
+]
